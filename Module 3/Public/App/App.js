1c4bc42efc57a80889b184d9fb778c6637c7c1c0
Maurice de Beijer <mauricedb@computer.org>
2014-09-29 17:10:54 -0400

    Module 3 - Directives

---
+++ Module 3/Public/App/App.js
@@ -0,0 +1,43 @@
+﻿(function() {
+    var app = angular.module("demoApp", []);
+
+    app.controller("demoCtrl", function($scope) {
+
+    });
+
+
+    app.directive("myFocus", function() {
+
+        return {
+            scope: true,
+            link: function(scope, element) {
+                element[0].focus();
+
+                element.bind("click", function()
+                {});
+
+                element.on("$destroy", function() {
+                    element.unbind("click");
+                });
+            }
+        }
+    })
+
+
+    app.directive("myTemplate", function() {
+
+        return {
+            template: "<span>A new span<span ng-transclude=true></span></span>",
+            replace: true,
+            transclude: true,
+            require:"?ngModel",
+            link:function(scope, element) {
+                console.log(scope.n)
+
+                if (scope.n % 2 == 0) {
+                    element.addClass("even");
+                }
+            }
+        }
+    });
+}());
