b2e2bc499dd5fb69370805c3ce63514474e6a719
Maurice de Beijer <mauricedb@computer.org>
2014-09-30 15:20:43 -0400

    Module 6 - AngularJS Internals

---
+++ Module 6/Public/App/App.js
@@ -0,0 +1,60 @@
+﻿var app = angular.module("myApp", []);
+
+app.controller("demoCtrl", function ($scope, $sce) {
+    console.log($scope);
+
+    $scope.snippet = $sce.trustAsHtml("<div>Data</div>");
+});
+
+
+app.directive("myDirective1", function () {
+    return {
+        scope: {},
+        link: function () {
+            console.log("myDirective1");
+        }
+    }
+})
+
+app.directive("myDirective2", function () {
+    return {
+        scope: {},
+        link: function () {
+            console.log("myDirective2");
+        }
+    }
+})
+
+
+//var o = { x: 1 };
+
+
+//var watches = [
+//    {
+//        oldValue: undefined,
+//        watch: function () {
+//            return o.x;
+//        }
+//    }
+//];
+
+//function digest() {
+//    watches.forEach(function(watch) {
+//        var current = watch.watch();
+//        if (current !== watch.oldValue) {
+//            console.log(watch.oldValue, current);
+//            watch.oldValue = current;
+//        }
+//    })
+//}
+
+
+////Object.observe(o, function(e) {
+////    console.log(e);
+////});
+
+//digest();
+//o.x = 2;
+//digest();
+//o.y = 3;
+//digest();
